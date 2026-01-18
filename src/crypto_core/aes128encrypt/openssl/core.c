8da6de3768491c93d104cb4cc67cc39169d7df40
Stefano Trevisani <stefanotrevisani98@gmail.com>
2026-01-18 19:17:49 +0100

    updated

--- src/crypto_core/aes128encrypt/openssl/core.c
+++ src/crypto_core/aes128encrypt/openssl/core.c
@@ -1,4 +1,4 @@
-#include <openssl/aes.h>
+#include <openssl/evp.h>
 #include "crypto_core_aes128encrypt.h"
 
 int crypto_core_aes128encrypt_openssl(
@@ -8,8 +8,24 @@ int crypto_core_aes128encrypt_openssl(
   const unsigned char *c
 )
 {
-  AES_KEY kexp;
-  AES_set_encrypt_key(k,128,&kexp);
-  AES_encrypt(in,out,&kexp);
+  EVP_CIPHER_CTX *ctx;
+  int outlen;
+
+  ctx = EVP_CIPHER_CTX_new();
+  if (!ctx) return -1;
+
+  if (EVP_EncryptInit_ex(ctx, EVP_aes_128_ecb(), NULL, k, NULL) != 1) {
+    EVP_CIPHER_CTX_free(ctx);
+    return -1;
+  }
+
+  EVP_CIPHER_CTX_set_padding(ctx, 0);
+
+  if (EVP_EncryptUpdate(ctx, out, &outlen, in, 16) != 1) {
+    EVP_CIPHER_CTX_free(ctx);
+    return -1;
+  }
+
+  EVP_CIPHER_CTX_free(ctx);
   return 0;
 }
